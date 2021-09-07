cec8eee38c0cc6a05bb4c868f4f141073b1a58e6
Dario Tecchia <dariotecchia@gmail.com>
2021-09-07 16:18:05 +0200

    add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container class="mt-4">
+    <b-jumbotron
+      header="DARTStats"
+      lead="Stats pot for DART smell detector plugin"
+    >
+      <b-breadcrumb class="p-0 m-0" :items="formatPath"></b-breadcrumb>
+    </b-jumbotron>
+    <Nuxt />
+  </b-container>
+</template>
+
+<script>
+export default {
+  computed: {
+    formatPath() {
+      let tempPath = "";
+      return this.$route.path.split("/").map((elem) => {
+        tempPath += elem + "/";
+        return ({
+          text: elem != "" ? elem : "home",
+          href: tempPath,
+        });
+      });
+    }
+  }
+};
+</script>
