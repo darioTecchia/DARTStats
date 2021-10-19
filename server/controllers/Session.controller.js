9609d30e7477bfadb97c59e40b54c3dd13239c0e
Dario Tecchia <dariotecchia@gmail.com>
2021-10-19 18:39:09 +0200

    update Session controller implementation

--- server/controllers/Session.controller.js
+++ server/controllers/Session.controller.js
@@ -1,52 +1,58 @@
 const db = require("../models");
-const Stat = db.Stat;
 const Session = db.Session;
 
 // Retrieve all Sessions from the database.
-exports.findAll = (req, res) => {
-  Session.find()
-    .then(data => {
-      delete data.sessions;
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving stats."
-      });
+exports.findAll = async (req, res) => {
+  // create condition object
+
+  try {
+    const data = await Session.find();
+    delete data.sessions;
+    res.send(data);
+    return data;
+  } catch (error) {
+    res.status(500).send({
+      message:
+        error.message || "Some error occurred while retrieving stats."
     });
+    throw error;
+  }
 };
 
 // Find a single Session with an id
-exports.findOne = (req, res) => {
+exports.findOne = async (req, res) => {
   const id = req.params.id;
 
-  Session.findById(id)
-    .deepPopulate("actions")
-    .then(data => {
-      if (!data)
-        res.status(404).send({ message: "Not found Session with id " + id });
-      else res.send(data);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .send({ message: "Error retrieving Session with id=" + id });
-    });
+  try {
+    const data = await Session.findById(id);
+    if (!data) {
+      res.status(404).send({ message: "Not found Session with id " + id });
+      throw new Error("Not found Session with id " + id)
+    }
+    else {
+      res.send(data);
+      return data;
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Error retrieving Session with id=" + id });
+    throw error;
+  }
 };
 
 // Delete all Sessions from the database.
-exports.deleteAll = (req, res) => {
-  Session.deleteMany({})
-    .then(data => {
-      res.send({
-        message: `${data.deletedCount} Sessions were deleted successfully!`
-      });
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while removing all stats."
-      });
+exports.deleteAll = async (req, res) => {
+
+  try {
+    const data = await Session.deleteMany({});
+    res.send({
+      message: `${data.deletedCount} Sessions were deleted successfully!`
     });
+    return { deleted: data.deletedCount };
+  } catch (error) {
+    res.status(500).send({
+      message:
+        error.message || "Some error occurred while removing all stats."
+    });
+    throw error;
+  }
 };
