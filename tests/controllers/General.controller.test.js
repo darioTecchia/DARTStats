ca64d7fd6d7fa1607e673904977f2681185a9548
Dario Tecchia <dariotecchia@gmail.com>
2021-10-19 18:43:18 +0200

    add General controller test

---
+++ tests/controllers/General.controller.test.js
@@ -0,0 +1,29 @@
+const GeneralController = require("../../server/controllers/General.controller");
+
+const db = require('../db');
+
+let stat;
+
+beforeAll(async () => await db.connect())
+beforeEach(async () => stat = await db.crateStat())
+afterEach(async () => await db.clearDatabase())
+afterAll(async () => await db.closeDatabase())
+
+describe('General Controller Tests', () => {
+
+  test('general', async () => {
+    const req = db.mockRequest({
+      params: {
+        id: stat._id
+      }
+    });
+    const res = db.mockResponse();
+    const general = await GeneralController.general(req, res);
+    console.log(general);
+    expect(general).toBeTruthy();
+    expect(general.statCount).toBe(1);
+    expect(general.sessionStructuralCount).toBe(1);
+    expect(general.sessionTextualCount).toBe(1);
+    expect(general.actionCount).toBe(5);
+  })
+})
