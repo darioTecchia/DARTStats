a8f6209ef0fea7c5cce8a613b505473c03e38215
Dario Tecchia <dariotecchia@gmail.com>
2021-10-22 15:23:21 +0200

    add session and action findOne tests

--- tests/controllers/Action.controller.test.js
+++ tests/controllers/Action.controller.test.js
@@ -5,12 +5,52 @@ const ActionController = require("../../server/controllers/Action.controller");
 
 const db = require('../db');
 
-let stat;
+let stat, action;
 
 beforeAll(async () => await db.connect())
 beforeEach(async () => stat = await db.crateStat())
 afterEach(async () => await db.clearDatabase())
 afterAll(async () => await db.closeDatabase())
+
+describe('Action Find Controller Tests', () => {
+  beforeEach(async () => {
+    stat = await db.crateStat()
+    action = new Action({
+      "actionKind": "REFACTORING_PREVIEW",
+      "smellKind": "GENERAL_FIXTURE",
+      "timestamp": 0,
+      "className": "TestCreazioneAnnuncio",
+      "methodName": "testCreazioneAnnuncio",
+      "packageName": "gestioneAnnuncio",
+      "actionCanceled": false,
+      "actionDone": true,
+      "statId": stat._id.toString(),
+      "sessionId": "41224d776a326fb40f000001"
+    });
+    await action.save(action);
+  });
+
+  test('findOne', async () => {
+    const req = db.mockRequest({
+      params: {
+        id: action._id
+      }
+    });
+    const res = db.mockResponse();
+    const singleAction = await ActionController.findOne(req, res);
+    expect(singleAction).toBeTruthy();
+    expect(singleAction.actionKind).toBe("REFACTORING_PREVIEW");
+    expect(singleAction.smellKind).toBe("GENERAL_FIXTURE");
+    expect(singleAction.timestamp).toBe(0);
+    expect(singleAction.className).toBe("TestCreazioneAnnuncio");
+    expect(singleAction.methodName).toBe("testCreazioneAnnuncio");
+    expect(singleAction.packageName).toBe("gestioneAnnuncio");
+    expect(singleAction.actionCanceled).toBe(false);
+    expect(singleAction.actionDone).toBe(true);
+    expect(singleAction.statId.toString()).toBe(stat._id.toString());
+    expect(singleAction.sessionId.toString()).toBe("41224d776a326fb40f000001");
+  })
+})
 
 describe('Action Controller Tests', () => {
 
@@ -20,23 +60,6 @@ describe('Action Controller Tests', () => {
     const actions = await ActionController.findAll(req, res);
     expect(actions).toHaveLength(5);
   })
-
-  // test('findOne', async () => {
-
-
-  //   const req = db.mockRequest({
-  //     params: {
-  //       id: stat._id
-  //     }
-  //   });
-  //   const res = db.mockResponse();
-  //   const singleStat = await ActionController.findOne(req, res);
-  //   expect(singleStat).toBeTruthy();
-  //   expect(singleStat.sessions).toHaveLength(2);
-  //   expect(singleStat.nOfExecutionTextual).toBe(1);
-  //   expect(singleStat.nOfExecutionStructural).toBe(1);
-  //   expect(singleStat.nOfTotalExecution).toBe(2);
-  // })
 
   test('findOne [wrong ID]', async () => {
     const req = db.mockRequest({
--- tests/controllers/Session.controller.test.js
+++ tests/controllers/Session.controller.test.js
@@ -5,12 +5,57 @@ const SessionController = require("../../server/controllers/Session.controller")
 
 const db = require('../db');
 
-let stat;
+let stat, session;
 
 beforeAll(async () => await db.connect())
 beforeEach(async () => stat = await db.crateStat())
 afterEach(async () => await db.clearDatabase())
 afterAll(async () => await db.closeDatabase())
+
+describe('Session Find Controller Tests', () => {
+  beforeEach(async () => {
+    stat = await db.crateStat()
+    session = new Session({
+      "userId": "851B0DF4C83AAC9273C014C57B127AF7",
+      "projectName": "SorrentoMarina",
+      "startTime": 1630919595704,
+      "endTime": 1630919638780,
+      "nOfGF": 61,
+      "nOfET": 27,
+      "nOfLOC": 61,
+      "nOfTotalClasses": 167,
+      "kind": "Textual",
+      "statId": stat._id.toString(),
+      "actions": [
+        "41224d776a326fb40f000001",
+        "41224d776a326fb40f000002"
+      ]
+    });
+    await session.save(session);
+  });
+
+  test('findOne', async () => {
+    const req = db.mockRequest({
+      params: {
+        id: session._id
+      }
+    });
+    const res = db.mockResponse();
+    const singleSession = await SessionController.findOne(req, res);
+    expect(singleSession).toBeTruthy();
+    expect(singleSession.userId).toBe("851B0DF4C83AAC9273C014C57B127AF7");
+    expect(singleSession.projectName).toBe("SorrentoMarina");
+    expect(singleSession.startTime).toBe(1630919595704);
+    expect(singleSession.endTime).toBe(1630919638780);
+    expect(singleSession.nOfGF).toBe(61);
+    expect(singleSession.nOfET).toBe(27);
+    expect(singleSession.nOfLOC).toBe(61);
+    expect(singleSession.nOfTotalClasses).toBe(167);
+    expect(singleSession.kind).toBe("Textual");
+    expect(singleSession.statId.toString()).toBe(stat._id.toString());
+    expect(singleSession.actions).toHaveLength(2);
+  })
+})
 
 describe('Session Controller Tests', () => {
 
