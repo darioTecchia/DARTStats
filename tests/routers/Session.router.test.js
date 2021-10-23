045a54c58c9a8e0ec1ceadc809c23188d5f06e73
Dario Tecchia <dariotecchia@gmail.com>
2021-10-23 12:28:06 +0200

    update session router test, add session api find test

--- tests/routers/Session.router.test.js
+++ tests/routers/Session.router.test.js
@@ -1,49 +1,82 @@
+const models = require('../../server/models');
+const Session = models.Session;
+
 const serverScaffold = require('../serverScaffold');
 const supertest = require('supertest');
 
-let stat, app;
+let stat, session, app;
 
 beforeAll(async () => app = await serverScaffold.connect())
 beforeEach(async () => stat = await serverScaffold.populateDb())
 afterEach(async () => await serverScaffold.clearDatabase())
 afterAll(async () => await serverScaffold.closeDatabase())
 
-describe('Session API Tests', () => {
-  test('GET /api/action', async () => {
+describe('Session API Find Tests', () => {
+  beforeEach(async () => {
+    stat = await serverScaffold.populateDb()
+    session = new Session({
+      "userId": "851B0DF4C83AAC9273C014C57B127AF7",
+      "projectName": "SorrentoMarina",
+      "startTime": 1630919595704,
+      "endTime": 1630919638780,
+      "nOfGF": 61,
+      "nOfET": 27,
+      "nOfLOC": 61,
+      "nOfTotalClasses": 167,
+      "kind": "Textual",
+      "statId": stat._id.toString(),
+      "actions": [
+        "41224d776a326fb40f000001",
+        "41224d776a326fb40f000002"
+      ]
+    });
+    await session.save(session);
+  });
+
+  test('GET /api/session/:id', async () => {
     const response = await supertest(app)
-      .get('/api/action')
+      .get('/api/session/' + session._id.toString())
       .expect(200)
-    expect(response.body).toHaveLength(5);
+    expect(response.body).toBeTruthy();
+    expect(response.body.userId).toBe("851B0DF4C83AAC9273C014C57B127AF7");
+    expect(response.body.projectName).toBe("SorrentoMarina");
+    expect(response.body.startTime).toBe(1630919595704);
+    expect(response.body.endTime).toBe(1630919638780);
+    expect(response.body.nOfGF).toBe(61);
+    expect(response.body.nOfET).toBe(27);
+    expect(response.body.nOfLOC).toBe(61);
+    expect(response.body.nOfTotalClasses).toBe(167);
+    expect(response.body.kind).toBe("Textual");
+    expect(response.body.statId.toString()).toBe(stat._id.toString());
+    expect(response.body.actions).toHaveLength(2);
+  })
+})
+
+describe('Session API Tests', () => {
+  test('GET /api/session', async () => {
+    const response = await supertest(app)
+      .get('/api/session')
+      .expect(200)
+    expect(response.body).toHaveLength(2);
   })
 
-  // test('GET /api/action/:id', async () => {
-  //   const response = await supertest(app)
-  //     .get('/api/action/' + stat._id.toString())
-  //     .expect(200)
-  //   expect(response.body).toBeTruthy();
-  //   expect(response.body.sessions).toHaveLength(2);
-  //   expect(response.body.nOfExecutionTextual).toBe(1);
-  //   expect(response.body.nOfExecutionStructural).toBe(1);
-  //   expect(response.body.nOfTotalExecution).toBe(2);
-  // })
-
-  test('GET /api/action/:id [wrong ID]', async () => {
+  test('GET /api/session/:id [wrong ID]', async () => {
     const response = await supertest(app)
-      .get('/api/action/41224d776a326fb40f000001')
+      .get('/api/session/41224d776a326fb40f000001')
       .expect(404);
   })
 
-  test('GET /api/action/:id [invalid ID]', async () => {
+  test('GET /api/session/:id [invalid ID]', async () => {
     const response = await supertest(app)
-      .get('/api/action/123')
+      .get('/api/session/123')
       .expect(500);
   })
 
-  test('DELETE /api/action', async () => {
+  test('DELETE /api/session', async () => {
     const response = await supertest(app)
-      .delete('/api/action')
+      .delete('/api/session')
       .expect(200)
     expect(response.body).toBeTruthy();
-    expect(response.body.message).toBe('5 Actions were deleted successfully!')
+    expect(response.body.message).toBe('2 Sessions were deleted successfully!')
   })
 })
